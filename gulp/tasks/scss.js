22551658cbcd683562154d3eb8587b5952f695e6
Serhii Kulynets <kylun1serg@gmail.com>
2023-03-30 13:45:45 +0300

    add order, about, delivery, contact, autorization, profile(order history) pages / add source map

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -5,11 +5,18 @@ import cleanCss from 'gulp-clean-css';
 import webpcss from 'gulp-webpcss';
 import autoprefixer from 'gulp-autoprefixer';
 import groupCssMediaQueries from 'gulp-group-css-media-queries';
-
+import sourcemaps from 'gulp-sourcemaps';
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-    return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+    // return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+    return app.gulp.src(app.path.src.scss)
+        .pipe(
+            app.plugins.if(
+                app.isDev,
+                sourcemaps.init()
+            )
+        )
         .pipe(app.plugins.plumber(
             app.plugins.notify.onError({
                 title: 'SCSS',
@@ -27,15 +34,15 @@ export const scss = () => {
                 groupCssMediaQueries()
             )
         )
-        // .pipe(
-        //     app.plugins.if(
-        //         app.isBuild,
-        //         webpcss({
-        //             webpClass: '.webp',
-        //             noWebpClass: '.no-webp'
-        //         })
-        //     )
-        // )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                webpcss({
+                    webpClass: '.webp',
+                    noWebpClass: '.no-webp'
+                })
+            )
+        )
         .pipe(
             app.plugins.if(
                 app.isBuild,
@@ -46,11 +53,17 @@ export const scss = () => {
                 })
             )
         )
-        .pipe(app.gulp.dest(app.path.build.css))
+        // .pipe(app.gulp.dest(app.path.build.css))
         .pipe(
             app.plugins.if(
                 app.isBuild,
                 cleanCss()
+            )
+        )
+        .pipe(
+            app.plugins.if(
+                app.isDev,
+                sourcemaps.write()
             )
         )
         .pipe(rename({
